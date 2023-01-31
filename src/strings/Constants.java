f4e1c1071d4ef332f0c4a25eb5d179cb16719db8
gtom06 <56637690+gtom06@users.noreply.github.com>
2023-01-31 10:30:24 +0100

    fix

---
+++ src/strings/Constants.java
@@ -0,0 +1,14 @@
+package strings;
+
+public final class Constants {
+
+    private Constants() {
+        throw new IllegalStateException();
+    }
+
+    public static final String RUN_OK_1 = "Application 1 started";
+    public static final String RUN_OK_2 = "Application 2 started";
+
+    public static final String INSERT = "\nInserisci un numero";
+    public static final String RESULT = "\nRisultato:\n";
+}
